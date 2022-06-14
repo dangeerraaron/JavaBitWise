7e7bce6fd97026a139a86a64833f85f98241461c
Aaron J Lanning <64545232+dangeerraaron@users.noreply.github.com>
2022-06-13 21:01:52 -0400

    Add files via upload
    Bit wise XOR operation

---
+++ bitwiseXOR.java
@@ -0,0 +1,11 @@
+class bitwiseXOR{
+    public static void main(String[] args) {
+        int x=0b1010;
+        int y=0b0110;
+        int z;
+        
+        z = x ^ y;
+        
+        System.out.println(z); 
+    }
+}
